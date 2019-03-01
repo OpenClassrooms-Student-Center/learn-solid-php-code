c32581f099e54e3402c04abc15d8505734d52714
Mickaël Andrieu <andrieu.travail@gmail.com>
2019-03-01 17:14:27 +0100

    Make application configurable outside of git

--- config/config_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('SERVER', '127.0.0.1');
-define('USER', 'root');
-define('PASSWD', 'password');
-define('DB_NAME', 'we_play_music');
-
-define('PORT', '3306');
-define('PDO_DSN', 'mysql:host=' . SERVER . ';port=' . PORT . ';dbname=' . DB_NAME);
