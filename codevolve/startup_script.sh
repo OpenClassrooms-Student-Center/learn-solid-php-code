4d1671e94fd368a2702ac4d35d44b64f0e4973b8
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-03-10 09:46:02 +0100

    Added Codevolve configuration

---
+++ codevolve/startup_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Needs to be copied into "Startup Script" section of Settings.
+if [ -e /var/www/html/codevolve/apache/default.conf ]
+then
+    cp /var/www/html/codevolve/apache/default.conf /etc/apache2/sites-enabled/
+    rm /etc/apache2/sites-enabled/000-default.conf
+    sudo service apache2 restart
+fi
+
+touch /root/.bashrcs/setBashDirectory.sh
+printf '#!bin/bash\ncd /var/www/html' >> /root/.bashrcs/setBashDirectory.sh
